42934089e9c411319aca5abcd5b5663aa1178798
Zhulingru <82667369+Zhulingru@users.noreply.github.com>
2024-01-05 21:03:33 +0800

    改一點點

--- main.ts
+++ main.ts
@@ -4,6 +4,36 @@ namespace SpriteKind {
     export const NPC = SpriteKind.create()
     export const 水 = SpriteKind.create()
 }
+function NPClist () {
+    薛之交 = sprites.create(assets.image`薛之交`, SpriteKind.NPC)
+    火鍋店家 = sprites.create(assets.image`火鍋店家`, SpriteKind.NPC)
+    冰淇淋店家 = sprites.create(assets.image`冰淇淋店家`, SpriteKind.NPC)
+    小明 = sprites.create(assets.image`小明`, SpriteKind.NPC)
+    小王 = sprites.create(assets.image`小王`, SpriteKind.NPC)
+    小白 = sprites.create(assets.image`小白`, SpriteKind.NPC)
+    小黑 = sprites.create(assets.image`小黑`, SpriteKind.NPC)
+    小黃 = sprites.create(assets.image`小黃`, SpriteKind.NPC)
+    全部的角色 = [
+    薛之交,
+    火鍋店家,
+    冰淇淋店家,
+    小明,
+    小王,
+    小白,
+    小黑,
+    小黃
+    ]
+}
+function NPClocation () {
+    tiles.placeOnTile(薛之交, tiles.getTileLocation(5, 5))
+    tiles.placeOnTile(火鍋店家, tiles.getTileLocation(6, 6))
+    tiles.placeOnTile(冰淇淋店家, tiles.getTileLocation(5, 16))
+    tiles.placeOnTile(小明, tiles.getTileLocation(10, 20))
+    tiles.placeOnTile(小王, tiles.getTileLocation(20, 3))
+    tiles.placeOnTile(小白, tiles.getTileLocation(18, 8))
+    tiles.placeOnTile(小黑, tiles.getTileLocation(12, 2))
+    tiles.placeOnTile(小黃, tiles.getTileLocation(12, 16))
+}
 controller.player2.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Released, function () {
     if (!(tiles.tileAtLocationIsWall(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom)))) {
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setImage(assets.image`角色2下`)
@@ -13,7 +43,27 @@ controller.player2.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Re
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC, function (sprite, otherSprite) {
-	
+    if (otherSprite == 薛之交) {
+        story.spriteSayText(otherSprite, "我想吃火鍋，也想吃冰淇淋", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 火鍋店家) {
+        story.spriteSayText(otherSprite, "我這裡有熱騰騰的火鍋", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 冰淇淋店家) {
+        story.spriteSayText(otherSprite, "我有好吃的冰淇淋", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 小王) {
+        story.spriteSayText(otherSprite, "想要渡海，就必須要有船", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 小白) {
+        story.spriteSayText(otherSprite, "東南方的海域很可怕", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 小明) {
+        story.spriteSayText(otherSprite, "你們看起來不像村裡的人", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 小黃) {
+        story.spriteSayText(otherSprite, "最近的洪患真可怕", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 小黑) {
+        story.spriteSayText(otherSprite, "好多天沒吃東西了", 15, 1, story.TextSpeed.Slow)
+    } else if (otherSprite == 水) {
+    	
+    } else {
+    	
+    }
 })
 controller.player3.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Released, function () {
     if (!(tiles.tileAtLocationIsWall(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)).tilemapLocation().getNeighboringLocation(CollisionDirection.Right)))) {
@@ -72,12 +122,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.水, function (sprite, otherSpri
     otherSprite.setFlag(SpriteFlag.Invisible, false)
     game.showLongText("這裡是深海，看起來需要什麼才能過去。。。", DialogLayout.Center)
 })
-function 所有NPC建立角色效果初始化 (角色們: Sprite[]) {
-    for (let index = 0; index <= 角色們.length - 1; index++) {
-        角色們[index].setFlag(SpriteFlag.Ghost, true)
-        角色們[index].setFlag(SpriteFlag.Invisible, true)
-    }
-}
 controller.player4.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Released, function () {
     scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)))
 })
@@ -89,6 +133,18 @@ controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Rele
         pause(200)
     }
 })
+function startvariable () {
+    玩家一Ｘ = 1
+    玩家一Ｙ = 1
+    玩家二Ｘ = 24
+    玩家二Ｙ = 2
+    玩家三Ｘ = 2
+    玩家三Ｙ = 21
+    玩家四Ｘ = 11
+    玩家四Ｙ = 6
+    誰踩到 = 0
+    有沒有人離開了 = 0
+}
 controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Released, function () {
     if (!(tiles.tileAtLocationIsWall(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).tilemapLocation().getNeighboringLocation(CollisionDirection.Left)))) {
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setImage(assets.image`角色2左`)
@@ -97,26 +153,6 @@ controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Re
         pause(200)
     }
 })
-function NPC清單 () {
-    薛之交 = sprites.create(assets.image`薛之交`, SpriteKind.NPC)
-    火鍋店家 = sprites.create(assets.image`火鍋店家`, SpriteKind.NPC)
-    冰淇淋店家 = sprites.create(assets.image`冰淇淋店家`, SpriteKind.NPC)
-    小明 = sprites.create(assets.image`小明`, SpriteKind.NPC)
-    小王 = sprites.create(assets.image`小王`, SpriteKind.NPC)
-    小白 = sprites.create(assets.image`小白`, SpriteKind.NPC)
-    小黑 = sprites.create(assets.image`小黑`, SpriteKind.NPC)
-    小黃 = sprites.create(assets.image`小黃`, SpriteKind.NPC)
-    全部的角色 = [
-    小王,
-    冰淇淋店家,
-    火鍋店家,
-    小黑,
-    小明,
-    小白,
-    薛之交,
-    小黃
-    ]
-}
 controller.player3.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Released, function () {
     scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)))
 })
@@ -128,15 +164,6 @@ controller.player3.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Re
         pause(200)
     }
 })
-function ＮＰＣ屬性表 (角色Ｘ座標: number, 角色Ｙ座標: number, 有沒有踩到: number, 有沒有離開: number) {
-    ＮＰＣ屬性表(1, 1, 1, 1)
-    薛之交屬性 = [
-    5,
-    5,
-    0,
-    0
-    ]
-}
 controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Released, function () {
     if (!(tiles.tileAtLocationIsWall(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).tilemapLocation().getNeighboringLocation(CollisionDirection.Top)))) {
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setImage(assets.image`角色2上`)
@@ -167,15 +194,15 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖1`, function (spr
     game.showLongText("可以渡海了！", DialogLayout.Bottom)
     game.splash("一群人遇到什麼樣的冒險呢？")
 })
-function 所有NPC的位置 () {
-    tiles.placeOnTile(薛之交, tiles.getTileLocation(5, 5))
-    tiles.placeOnTile(火鍋店家, tiles.getTileLocation(6, 6))
-    tiles.placeOnTile(冰淇淋店家, tiles.getTileLocation(5, 16))
-    tiles.placeOnTile(小王, tiles.getTileLocation(20, 3))
-    tiles.placeOnTile(小白, tiles.getTileLocation(18, 8))
-    tiles.placeOnTile(小明, tiles.getTileLocation(10, 20))
-    tiles.placeOnTile(小黃, tiles.getTileLocation(12, 16))
-    tiles.placeOnTile(小黑, tiles.getTileLocation(12, 2))
+function blacksheepwall () {
+    Ｘ軸 = 0
+    Ｙ軸 = 0
+    for (let Ｙ軸 = 0; Ｙ軸 <= 23; Ｙ軸++) {
+        for (let Ｘ軸 = 0; Ｘ軸 <= 34; Ｘ軸++) {
+            迷霧角色 = sprites.create(assets.image`我的影像3`, SpriteKind.迷霧)
+            tiles.placeOnTile(迷霧角色, tiles.getTileLocation(Ｘ軸 + 1, Ｙ軸 + 1))
+        }
+    }
 }
 controller.player1.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Released, function () {
     if (!(tiles.tileAtLocationIsWall(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).tilemapLocation().getNeighboringLocation(CollisionDirection.Bottom)))) {
@@ -201,21 +228,6 @@ controller.player3.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Rele
         pause(200)
     }
 })
-function 新增迷霧 () {
-    Ｘ軸 = 0
-    Ｙ軸 = 0
-}
-function 玩家與遊戲初始化 () {
-    tiles.setCurrentTilemap(tilemap`背景1`)
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`我的影像11`, SpriteKind.Player))
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`角色2`, SpriteKind.Player))
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`角色3`, SpriteKind.Player))
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`角色4`, SpriteKind.Player))
-    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), tiles.getTileLocation(玩家一Ｘ, 玩家一Ｙ))
-    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), tiles.getTileLocation(玩家二Ｘ, 玩家二Ｙ))
-    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)), tiles.getTileLocation(玩家三Ｘ, 玩家三Ｙ))
-    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)), tiles.getTileLocation(玩家四Ｘ, 玩家四Ｙ))
-}
 controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Released, function () {
     if (!(tiles.tileAtLocationIsWall(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).tilemapLocation().getNeighboringLocation(CollisionDirection.Left)))) {
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).setImage(assets.image`角色1左`)
@@ -246,10 +258,21 @@ controller.player4.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Rele
 controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Released, function () {
     scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)))
 })
+function player2 () {
+    tiles.setCurrentTilemap(tilemap`背景1`)
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`我的影像11`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`角色2`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`角色3`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`角色4`, SpriteKind.Player))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), tiles.getTileLocation(玩家一Ｘ, 玩家一Ｙ))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), tiles.getTileLocation(玩家二Ｘ, 玩家二Ｙ))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)), tiles.getTileLocation(玩家三Ｘ, 玩家三Ｙ))
+    tiles.placeOnTile(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)), tiles.getTileLocation(玩家四Ｘ, 玩家四Ｙ))
+}
 controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Released, function () {
     scene.cameraFollowSprite(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)))
 })
-function NPC出現 (NPCＸ: number, NPCＹ: number) {
+function NPCdetect (NPCＸ: number, NPCＹ: number) {
     角色座標陣列 = [
     玩家一Ｘ,
     玩家一Ｙ,
@@ -279,32 +302,27 @@ function NPC出現 (NPCＸ: number, NPCＹ: number) {
         }
     }
 }
-function 初始化變數 () {
-    玩家一Ｘ = 1
-    玩家一Ｙ = 1
-    玩家二Ｘ = 24
-    玩家二Ｙ = 2
-    玩家三Ｘ = 2
-    玩家三Ｙ = 21
-    玩家四Ｘ = 11
-    玩家四Ｙ = 6
-    誰踩到 = 0
-    有沒有人離開了 = 0
+function NPCobjectlist () {
+    薛之交屬性 = [
+    5,
+    5,
+    0,
+    0
+    ]
 }
-let 有沒有人離開了 = 0
-let 誰踩到 = 0
+function NPCstate (角色們: Sprite[]) {
+    for (let index = 0; index <= 角色們.length - 1; index++) {
+        角色們[index].setFlag(SpriteFlag.Ghost, true)
+        角色們[index].setFlag(SpriteFlag.Invisible, true)
+    }
+}
+let 薛之交屬性: number[] = []
 let 角色座標陣列: number[] = []
+let 迷霧角色: Sprite = null
 let Ｙ軸 = 0
 let Ｘ軸 = 0
-let 薛之交屬性: number[] = []
-let 小黃: Sprite = null
-let 小黑: Sprite = null
-let 小白: Sprite = null
-let 小王: Sprite = null
-let 小明: Sprite = null
-let 冰淇淋店家: Sprite = null
-let 火鍋店家: Sprite = null
-let 薛之交: Sprite = null
+let 有沒有人離開了 = 0
+let 誰踩到 = 0
 let 玩家一Ｙ = 0
 let 玩家一Ｘ = 0
 let 玩家四Ｘ = 0
@@ -313,13 +331,21 @@ let 玩家三Ｙ = 0
 let 玩家三Ｘ = 0
 let 玩家二Ｘ = 0
 let 玩家二Ｙ = 0
+let 小黃: Sprite = null
+let 小黑: Sprite = null
+let 小白: Sprite = null
+let 小王: Sprite = null
+let 小明: Sprite = null
+let 冰淇淋店家: Sprite = null
+let 火鍋店家: Sprite = null
+let 薛之交: Sprite = null
 let 水: Sprite = null
 let 全部的角色: Sprite[] = []
-初始化變數()
-玩家與遊戲初始化()
-所有NPC建立角色效果初始化(全部的角色)
-所有NPC的位置()
-新增迷霧()
+startvariable()
+player2()
+NPCstate(全部的角色)
+NPClocation()
+blacksheepwall()
 for (let index = 0; index <= 20; index++) {
     水 = sprites.create(img`
         9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
@@ -388,7 +414,4 @@ forever(function () {
     if (玩家一Ｘ == 1 && 玩家一Ｙ == 2 || 玩家一Ｘ == 2 && 玩家一Ｙ == 1) {
         story.spriteSayText(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), "這裡是哪裡，到處看看吧！", 15, 1, story.TextSpeed.Slow)
     }
-})
-forever(function () {
-	
 })
